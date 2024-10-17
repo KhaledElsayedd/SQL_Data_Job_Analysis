7be4d9362009a5b33cc0af4984bc51263e66e9b9
KhaledElsayedd <khaledelsayed142@yahoo.com>
2024-10-17 13:52:51 +0300

    new file:   Project_SQL/1_top_paying_jobs.sql

---
+++ Project_SQL/1_top_paying_jobs.sql
@@ -0,0 +1,18 @@
+/*
+ Question: What are the top paying data analyst jobs in Egypt?
+ - Discover the top paying jobs for data analyst's that are available in Egypt.
+ - Focusing on jobs with specified salaries (remove nulls).
+ */
+SELECT job_id,
+    job_title,
+    name AS company_name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg AS avg_yearly_salary,
+    job_posted_date::DATE
+FROM job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE job_title_short LIKE '%Data%Analyst%'
+    AND salary_year_avg IS NOT NULL
+    AND job_location LIKE '%Egypt%'
+ORDER BY avg_yearly_salary DESC;
